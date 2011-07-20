3a304a3564cc8dfc2a191f7e1e547c71e798af8c
Grégoire Paris <github.token>
2011-07-20 22:55:13 +0200

    add type argument

--- switch-init.py
+++ switch-init.py
@@ -4,6 +4,8 @@ import argparse
 parser = argparse.ArgumentParser(description='Initialize a switch project')
 parser.add_argument('project', nargs=1,
                     help='the project name')
+parser.add_argument('--type', nargs=1, choices=['symfony', 'magento'],
+                    help='the project type')
 
 args = parser.parse_args()
 
