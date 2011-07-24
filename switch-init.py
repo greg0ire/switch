b36f3cf7a5e6ea39644338b2e3592e5e70df22bd
Grégoire Paris <github.token>
2011-07-24 15:39:23 +0200

    replace the hardcoded possible types with something out of the types directory list

--- switch-init.py
+++ switch-init.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import argparse
-
+import glob
+import os
 parser = argparse.ArgumentParser(description='Initialize a switch project')
 parser.add_argument('project', nargs=1,
                     help='the project name')
-parser.add_argument('--type', nargs=1, choices=['symfony', 'magento'],
+parser.add_argument('--type', nargs=1, choices= [os.path.splitext(os.path.basename(x))[0] for x in glob.glob('types/*.sh')],
                     help='the project type')
 
 args = parser.parse_args()
---
+++ types/symfony.sh
@@ -0,0 +1,6 @@
+function cc()
+{
+  cdproj
+  ./symfony cc
+  cd -
+}
