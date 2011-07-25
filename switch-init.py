b04b337b381ccda1850abe1ab1ed88ec950c5311
Grégoire Paris <github.token>
2011-07-25 23:39:50 +0200

    make the project name optional, and set default values for the project name and the project directory

--- switch-init.py
+++ switch-init.py
@@ -3,10 +3,13 @@ import argparse
 import glob
 import os
 parser = argparse.ArgumentParser(description='Initialize a switch project')
-parser.add_argument('project', nargs=1,
-                    help='the project name')
+parser.add_argument('--project-name', nargs=1, 
+                    default=os.path.basename(os.getcwd()),
+                    help='the project name (defaults to the cwd name)')
 parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in glob.glob('types/*')],
                     help='the project type')
 
-args = parser.parse_args()
+parser.add_argument('--project-dir', nargs=1, default=os.getcwd(),
+                    help='the directory where the project is store (defaults to .)')
 
+args = parser.parse_args()
