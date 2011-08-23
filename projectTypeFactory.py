e60587d1a63d1ebe5286c9cfc064a10c109f55f1
Grégoire Paris <postmaster@greg0ire.fr>
2011-08-23 23:29:08 +0200

    add some documentation

--- projectTypeFactory.py
+++ projectTypeFactory.py
@@ -3,6 +3,9 @@ class ProjectTypeFactory:
   """This factory builds project type template classes"""
   @classmethod
   def getInstance(cls, projectType, direction, projectDir):
+    """This method finds a template class, initializes it with usual arguments
+
+    """
     snippetClassName = projectType.capitalize() + 'Snippet'
     snippetModule    = __import__('projectTypes.' + projectType, globals(), locals(), [snippetClassName])
     tpl              = getattr(snippetModule, snippetClassName)(direction, projectDir)
