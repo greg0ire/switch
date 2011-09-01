bb21a3c872eea97605b19b1eb4899eee87815b45
Grégoire Paris <postmaster@greg0ire.fr>
2011-09-01 23:57:53 +0200

    We now use the project type factory

--- projectTypes/symfony2.py
+++ projectTypes/symfony2.py
@@ -1,6 +1,3 @@
-import os
 from Cheetah.Template import Template
 class Symfony2Snippet(Template):
-  def __init__(self, filename):
-    self.filename = filename
-    Template.__init__(self, file=os.path.join(os.path.dirname(__file__), 'symfony2', self.filename + '.tmpl'))
+  pass
