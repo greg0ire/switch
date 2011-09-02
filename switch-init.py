75ca9f9c61bb01aced8377f97f4071a4762b0df8
grpar <grpar@smile.fr>
2011-09-02 10:17:12 +0200

    fix bug with list arguments

--- switch-init.py
+++ switch-init.py
@@ -6,24 +6,27 @@ import sys
 from projectTypeFactory import ProjectTypeFactory
 parser = argparse.ArgumentParser(description='Initialize a switch project')
 parser.add_argument('--project-name', nargs=1, 
-                    default=os.path.basename(os.getcwd()),
+                    default=[os.path.basename(os.getcwd())],
                     help='the project name (defaults to the cwd name)')
 parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in glob.glob(os.path.dirname(sys.argv[0]) + '/projectTypes/*')],
                     help='the project type')
 
-parser.add_argument('--project-dir', nargs=1, default=os.getcwd(),
+parser.add_argument('--project-dir', nargs=1, default=[os.getcwd()],
                     help='the directory where the project is stored (defaults to .)')
 
 args = parser.parse_args()
 
-switchProjectPath = os.path.expanduser(os.path.join('~', '.switch', args.project_name))
+projectDir  = os.path.abspath(args.project_dir[0])
+projectName = args.project_name[0] 
+
+switchProjectPath = os.path.expanduser(os.path.join('~', '.switch', projectName))
 if not os.path.exists(switchProjectPath):
   os.makedirs(switchProjectPath)
 
 #include the core snippet
 from core import CoreSnippet
 for filename in ('in', 'out'):
-  tpl = CoreSnippet(filename, args.project_dir)
+  tpl = CoreSnippet(filename, projectDir)
   open(os.path.join(switchProjectPath, filename + '.sh'), 'w').write(str(tpl))
 
 #include the type snippet
@@ -31,5 +34,5 @@ if args.type != None and len(args.type) > 0 :
   snippetClassName = args.type[0].capitalize() + 'Snippet'
   snippetModule = __import__('projectTypes.' + args.type[0], globals(), locals(), [snippetClassName])
   for filename in ('in', 'out'):
-    tpl = ProjectTypeFactory.getInstance(args.type[0], filename, args.project_dir)
+    tpl = ProjectTypeFactory.getInstance(args.type[0], filename, projectDir)
     open(os.path.join(switchProjectPath, filename + '.sh'), 'a').write(str(tpl))
