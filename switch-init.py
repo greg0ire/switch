f86466de0f772ab573967a1cfe33355a14bab2cf
Grégoire Paris <postmaster@greg0ire.fr>
2011-09-06 22:41:47 +0200

    filter using isdir()

--- switch-init.py
+++ switch-init.py
@@ -8,7 +8,8 @@ parser = argparse.ArgumentParser(description='Initialize a switch project')
 parser.add_argument('--project-name', nargs=1, 
                     default=[os.path.basename(os.getcwd())],
                     help='the project name (defaults to the cwd name)')
-parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in glob.glob(os.path.dirname(sys.argv[0]) + '/projectTypes/*')],
+parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in 
+  filter(os.path.isdir, glob.glob(os.path.dirname(sys.argv[0]) + '/projectTypes/*'))],
                     help='the project type')
 
 parser.add_argument('--project-dir', nargs=1, default=[os.getcwd()],
