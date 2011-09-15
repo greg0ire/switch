a304ca114808b4c17e9e92e9352e9e8d14bc4aca
Grégoire Paris <postmaster@greg0ire.fr>
2011-09-15 23:41:45 +0200

    use python idiom
    Learnt it in the pyramid tutorial. See explanations here :
    https://pylonsproject.org/projects/pyramid/dev/narr/firstapp.html#configurator-construction

--- switch-init.py
+++ switch-init.py
@@ -4,36 +4,37 @@ import glob
 import os
 import sys
 from projectTypeFactory import ProjectTypeFactory
-parser = argparse.ArgumentParser(description='Initialize a switch project')
-parser.add_argument('--project-name', nargs=1, 
-                    default=[os.path.basename(os.getcwd())],
-                    help='the project name (defaults to the cwd name)')
-parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in 
-  filter(os.path.isdir, glob.glob(os.path.dirname(sys.argv[0]) + '/projectTypes/*'))],
-                    help='the project type')
+if __name__ =='main':
+  parser = argparse.ArgumentParser(description='Initialize a switch project')
+  parser.add_argument('--project-name', nargs=1, 
+                      default=[os.path.basename(os.getcwd())],
+                      help='the project name (defaults to the cwd name)')
+  parser.add_argument('--type', nargs=1, choices= [os.path.basename(x) for x in 
+    filter(os.path.isdir, glob.glob(os.path.dirname(sys.argv[0]) + '/projectTypes/*'))],
+                      help='the project type')
 
-parser.add_argument('--project-dir', nargs=1, default=[os.getcwd()],
-                    help='the directory where the project is stored (defaults to .)')
+  parser.add_argument('--project-dir', nargs=1, default=[os.getcwd()],
+                      help='the directory where the project is stored (defaults to .)')
 
-args = parser.parse_args()
+  args = parser.parse_args()
 
-projectDir  = os.path.abspath(args.project_dir[0])
-projectName = args.project_name[0] 
+  projectDir  = os.path.abspath(args.project_dir[0])
+  projectName = args.project_name[0] 
 
-switchProjectPath = os.path.expanduser(os.path.join('~', '.switch', projectName))
-if not os.path.exists(switchProjectPath):
-  os.makedirs(switchProjectPath)
+  switchProjectPath = os.path.expanduser(os.path.join('~', '.switch', projectName))
+  if not os.path.exists(switchProjectPath):
+    os.makedirs(switchProjectPath)
 
-#include the core snippet
-from core import CoreSnippet
-for filename in ('in', 'out'):
-  tpl = CoreSnippet(filename, projectDir)
-  open(os.path.join(switchProjectPath, filename + '.sh'), 'w').write(str(tpl))
-
-#include the type snippet
-if args.type != None and len(args.type) > 0 :
-  snippetClassName = args.type[0].capitalize() + 'Snippet'
-  snippetModule = __import__('projectTypes.' + args.type[0], globals(), locals(), [snippetClassName])
+  #include the core snippet
+  from core import CoreSnippet
   for filename in ('in', 'out'):
-    tpl = ProjectTypeFactory.getInstance(args.type[0], filename, projectDir)
-    open(os.path.join(switchProjectPath, filename + '.sh'), 'a').write(str(tpl))
+    tpl = CoreSnippet(filename, projectDir)
+    open(os.path.join(switchProjectPath, filename + '.sh'), 'w').write(str(tpl))
+
+  #include the type snippet
+  if args.type != None and len(args.type) > 0 :
+    snippetClassName = args.type[0].capitalize() + 'Snippet'
+    snippetModule = __import__('projectTypes.' + args.type[0], globals(), locals(), [snippetClassName])
+    for filename in ('in', 'out'):
+      tpl = ProjectTypeFactory.getInstance(args.type[0], filename, projectDir)
+      open(os.path.join(switchProjectPath, filename + '.sh'), 'a').write(str(tpl))
