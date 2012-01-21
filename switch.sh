e9f5cfae6c7cb4c61e35944babc497ce8308b9a2
Grégoire Paris <postmaster@greg0ire.fr>
2012-01-22 00:46:39 +0100

    use variables as much as we can

--- switch.sh
+++ switch.sh
@@ -1,13 +1,15 @@
 function switch()
 {
   local usage="switch [project_name]"
+  local switchHome=$HOME/.switch
+  local switchSave=$switchHome/proj.save
   case $# in 
   0 )
-    if [[ -f ~/.switch/proj.save  ]]
+    if [[ -f $switchHome/proj.save  ]]
     then
       echo "Deselecting current project"
-      source ~/.switch/`cat ~/.switch/proj.save`/out.sh
-      rm ~/.switch/proj.save
+      source $switchHome/`cat ~/.switch/proj.save`/out.sh
+      rm $switchSave
       cd
     else
       echo "Current project not found">&2
@@ -17,14 +19,14 @@ function switch()
     if [[ -f ~/.switch/proj.save  ]]  
     then
       echo "Deselecting current project"
-      source ~/.switch/`cat ~/.switch/proj.save`/out.sh
-      rm ~/.switch/proj.save
+      source $switchHome/`cat ~/.switch/proj.save`/out.sh
+      rm $switchSave
     fi
-    if [[ -d ~/.switch/$1  ]]
+    if [[ -d $switchHome/$1  ]]
     then
       echo "Switching to project \"$1\""
-      source ~/.switch/$1/in.sh
-      echo "$1" > ~/.switch/proj.save
+      source $switchHome/$1/in.sh
+      echo "$1" > $switchSave
     else
       echo "\"$1\" is not a switch project. Available projects: `ls -x ~/.switch`">&2
     fi
